f01581bd1ceb96cf85a25aed6e4e33944b4b2c05
Munish <hmunish5@gmail.com>
2023-06-22 00:42:30 +0200

    Fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
-import {getScores, renderScores, submitScore} from './modules/functions.js'
-
+import { getScores, renderScores, submitScore } from './modules/functions.js';
 
 const submitForm = document.querySelector('#addScoreForm');
 const refreshScoreBtn = document.querySelector('.btn.refresh');
@@ -8,44 +7,43 @@ const scoreBox = document.querySelector('.scores');
 
 const gameId = 'Wg4I0z6OKTRJzU6wugcM';
 
-submitForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const scoreName = e.target.scoreName.value;
-    const score = e.target.score.value;
+submitForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const scoreName = e.target.scoreName.value;
+  const score = e.target.score.value;
 
-    e.target.scoreName.value = '';
-    e.target.score.value = '';
+  e.target.scoreName.value = '';
+  e.target.score.value = '';
 
-    (async ()=>{
-        try{
-            await submitScore(gameId, scoreName, score);
-            alert('Score added successfully, wait for scores to update');
-            const newScores = await getScores(gameId);
-            renderScores(scoreBox, newScores.result);
-        }catch(err){
-            console.error(err.message);
-        }
-    })()
-})
+  (async () => {
+    try {
+      await submitScore(gameId, scoreName, score);
+      const newScores = await getScores(gameId);
+      renderScores(scoreBox, newScores.result);
+    } catch (err) {
+    //   Error will be handled here
+    }
+  })();
+});
 
-refreshScoreBtn.addEventListener('click', ()=>{
-    (async ()=>{
-        try{
-            const res = await getScores(gameId);
-            renderScores(scoreBox, res.result);
-        }catch(err){
-            console.error(err.message);
-        }
-    })()
-})
+refreshScoreBtn.addEventListener('click', () => {
+  (async () => {
+    try {
+      const res = await getScores(gameId);
+      renderScores(scoreBox, res.result);
+    } catch (err) {
+      //   Error will be handled here
+    }
+  })();
+});
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    (async ()=>{
-        try{
-            const res = await getScores(gameId);
-            renderScores(scoreBox, res.result);
-        }catch(err){
-            console.error(err.message);
-        }
-    })()
-})
+document.addEventListener('DOMContentLoaded', () => {
+  (async () => {
+    try {
+      const res = await getScores(gameId);
+      renderScores(scoreBox, res.result);
+    } catch (err) {
+      //   Error will be handled here
+    }
+  })();
+});
